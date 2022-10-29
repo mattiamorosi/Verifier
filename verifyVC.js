3464e50cd18fdd76c95b38d5af9b2b8a467bc22e
mattiamorosi <66827085+mattiamorosi@users.noreply.github.com>
2022-10-29 19:05:29 +0200

    commit

--- verifyVC.js
+++ verifyVC.js
@@ -1,8 +1,9 @@
-const { verifyCredential } = require("@tanglelabs/identity-manager");
+const { verifyCredential, isCredentialValid } = require("@tanglelabs/identity-manager");
 const {Credential} = require("@iota/identity-wasm/node")
 
 const verifyVC = async (vc) => {
-    return await verifyCredential(Credential.fromJSON(vc));
+    return await isCredentialValid(Credential.fromJSON(vc));    // returns only VC
+    //return await verifyCredential(Credential.fromJSON(vc));   // returns both VC and DVID
     
 };
 
